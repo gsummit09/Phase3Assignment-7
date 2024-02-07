48c3bfc67e69670226c016b3f3465a0bd8253aa0
gsummit09 <nayaksumit998@gmail.com>
2024-02-07 16:24:47 +0530

    Add project files.

---
+++ Assignment07Feb07/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment07Feb07.Models;
+
+namespace Assignment07Feb07.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment07Feb07.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Assignment07Feb07/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment07Feb07.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string? MName { get; set; }
+        [StringLength(50)]
+        public string? StarCast { get; set; }
+        [StringLength(50)]
+        public string? Director { get; set; }
+        [StringLength(50)]
+        public string? Producer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
